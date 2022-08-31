a3d9e7e18fcdd65f83c4bc0145d9ce79d72cdc8b
GT-Zhang <1029550448@qq.com>
2022-08-31 18:38:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ from setuptools import find_packages
 
 __version__ = "0.6.3"
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.MD"), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
 # python setup.py sdist bdist_wheel
 setup(
     name='QGUI',
@@ -18,6 +21,8 @@ setup(
     author='GT-ZhangAcer',
     author_email='zhangacer@foxmail.com',
     description='QGUI - 0.1MB超轻量Python GUI框架，用模板来快捷制作深度学习模型推理界面 ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=["ttkbootstrap==0.5.1", "pillow>=8.2.0"],
     python_requires='>3.5',
     include_package_data=True,
